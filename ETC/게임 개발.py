a344f23dcb8978794dad1e20b9f9a4a984c64b04
Ella Ma <tiger980424@naver.com>
2022-01-02 16:09:33 +0900

    Update 게임 개발.py

--- ETC/게임 개발.py
+++ ETC/게임 개발.py
@@ -1,43 +1,47 @@
 import sys
 
-map_x, map_y = map(int, sys.stdin.readline().rstrip().split(' '))
-loca_x, loca_y, head = map(int, sys.stdin.readline().rstrip().split(' '))
+input = sys.stdin.readline
 
-moving_head= {0:3, 3:2, 2:1, 1:0}
-moving_dict = {0:[-1,0], 1:[0,-1], 2:[1,0], 3:[0,1]}
-moving_back = {0:[0,1], 1:[-1,0], 2:[0,1], 3:[1,0]}
+n,m = map(int,input().rstrip().split())
+nowy, nowx, nowd = map(int,input().rstrip().split())
 
-map_list = []
-extra =[0]*(map_y+2)
-map_list.append(extra)
-for i in range(map_y):
-    l = [0]
-    l.extend(list(map(int, sys.stdin.readline().rstrip().split(' '))))
-    l.append(0)
-    map_list.append(l)
+maps = [0] * n
 
-map_list.append(extra)
+for i in range(n):
+    maps[i] = list(map(int,input().rstrip().split()))
 
-map_list[loca_y][loca_x] = 2
-while(True):
-    moving_x, moving_y = moving_dict[head][0], moving_dict[head][1]
-    around=map_list[loca_y-1][loca_x-1 : loca_x+2]
-    around.extend(map_list[loca_y][loca_x-1 : loca_x+2])
-    around.extend(map_list[loca_y+1][loca_x-1 : loca_x+2])
+visitied = [[False for _ in range(m)] for _ in range(n)]
 
-    if around.count(1) == 0:
-        if map_list[moving_back[head][1]+loca_y][moving_back[head][0]+loca_x] == 0:
+moves = [(-1,0), (0,1), (1,0), (0,-1)]
+
+result = 1
+
+while True:
+    visitied[nowy][nowx] = True
+    check = False
+
+    for _ in range(4):
+        nowd = (nowd+3)%4
+
+        dy = nowy + moves[nowd][0]
+        dx = nowx + moves[nowd][1]
+
+        if dy<0 or dy>=n or dx<0 or dx>=m:
+            continue
+
+        if visitied[dy][dx] == False and maps[dy][dx] == 0:
+            nowy, nowx = dy, dx
+            check = True
+            result += 1
+            break
+
+    if check == False:
+        dy = nowy - moves[nowd][0]
+        dx = nowx - moves[nowd][1]
+
+        if maps[dy][dx] == 1:
             break
         else:
-            loca_y += moving_back[head][1]
-            loca_x += moving_back[head][0]
-    elif map_list[moving_y+loca_y][moving_x+loca_x] == 1:
-        loca_y += moving_y
-        loca_x += moving_x
-        head = moving_head[head]
-        map_list[loca_y][loca_x] = 2
+            nowy,nowx = dy,dx
 
-    elif map_list[moving_y+loca_y][moving_x+loca_x] == 2 or map_list[moving_y+loca_y][moving_x+loca_x] == 0:
-        head = moving_head[head]
-
-print(map_list[1].count(2)-1)
+print(result)
