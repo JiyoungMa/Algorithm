0098b939048097d976fd1caf732cbb56697d1f2c
Ella Ma <tiger980424@naver.com>
2022-03-10 00:00:21 +0900

    Update and rename 보석 쇼핑(ing).py to 보석 쇼핑.py

--- Programmers/보석 쇼핑.py
+++ Programmers/보석 쇼핑.py
@@ -4,12 +4,9 @@ def solution(gems):
     answer = [0,len(gems)-1]
     jewels_dict = dict()
 
-    while(start<= end and end<len(gems)):
-        if gems[end] in jewels_dict.keys():
-            jewels_dict[gems[end]] += 1
-        else:
-            jewels_dict[gems[end]] = 1
+    jewels_dict[gems[start]] = 1
 
+    while(start<= end and end<len(gems)):
         if len(jewels_dict.keys())==jewels:
             if answer[1]-answer[0] > end - start:
                 answer = [start,end]
@@ -19,6 +16,11 @@ def solution(gems):
             start += 1
         else:
             end += 1
+            if end<len(gems):
+                if gems[end] in jewels_dict.keys():
+                        jewels_dict[gems[end]] += 1
+                else:
+                    jewels_dict[gems[end]] = 1
 
     answer = [answer[0]+1, answer[1]+1]
     return answer
