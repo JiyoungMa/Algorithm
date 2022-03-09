50f5d1107f397e7f65a603d2d53c085b722c0899
Ella Ma <tiger980424@naver.com>
2022-03-09 23:53:36 +0900

    Create 보석 쇼핑(ing).py

---
+++ Programmers/보석 쇼핑(ing).py
@@ -0,0 +1,24 @@
+def solution(gems):
+    jewels = len(set(gems))
+    start, end = 0,0
+    answer = [0,len(gems)-1]
+    jewels_dict = dict()
+
+    while(start<= end and end<len(gems)):
+        if gems[end] in jewels_dict.keys():
+            jewels_dict[gems[end]] += 1
+        else:
+            jewels_dict[gems[end]] = 1
+
+        if len(jewels_dict.keys())==jewels:
+            if answer[1]-answer[0] > end - start:
+                answer = [start,end]
+
+        if start < end and jewels_dict[gems[start]] > 1:
+            jewels_dict[gems[start]] -= 1
+            start += 1
+        else:
+            end += 1
+
+    answer = [answer[0]+1, answer[1]+1]
+    return answer
