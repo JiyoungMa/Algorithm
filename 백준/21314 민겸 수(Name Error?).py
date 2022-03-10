28165a65018f10318461812b6e592758ce67e9b0
Ella Ma <tiger980424@naver.com>
2022-03-10 23:57:13 +0900

    Create 21314 민겸 수(Name Error?).py

---
+++ 백준/21314 민겸 수(Name Error?).py
@@ -0,0 +1,27 @@
+import sys
+
+strs = sys.stdin.readline().rstrip()
+
+maximum = ""
+minimum = ""
+
+string_split = strs.split("K")
+
+for s in range(len(string_split)-1):
+    now_str = string_split[s]
+    if len(now_str) == 0:
+        maximum += str(5)
+        minimum += str(5)
+    else:
+        mcount = len(string_split[s])
+        maximum += str(5*10**mcount)
+        minimum += str(10**(mcount-1)) + str(5)
+
+now_str = string_split[-1]
+if len(now_str) != 0 : 
+    mcount = len(string_split[s])
+    maximum += ''.join(["1"]*len(now_str))
+    minimum += str(10**(len(now_str)-1))
+
+print(maximum)
+print(minimum)
